342ea1090fc5e0ddb1573c8ae8b23f0995d1c901
Bar <Barg314@gmail.com>
2021-07-31 13:18:25 +0300

    Main source file

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "src/GeneratorFactory.hpp"
+#include "src/Options.hpp"
+
+int main(int argc, char** argv)
+{
+	Options options(argc, argv);
+	auto result = options.parse();
+
+	std::string lang = result[Options::LANG_OPTION].as<std::string>();
+	std::string name = result[Options::NAME_OPTION].as<std::string>();
+
+	GeneratorFactory generatorFactory;
+	std::unique_ptr<Generator> generator = generatorFactory.getGenerator(lang);
+
+	generator->setClassName(name);
+
+	if (result.count(Options::PRIVATE_OPTION))
+	{
+		generator->setPrivateMembers(result[Options::PRIVATE_OPTION].as<std::vector<std::string>>());
+	}
+
+	generator->generate();
+
+	return 1;
+}
